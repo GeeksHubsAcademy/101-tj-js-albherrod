b490d0411f93e7115ef43df205a5eedaf27603ef
ahernandezr05 <datw.alberto@gmail.com>
2021-07-28 17:48:55 +0200

    FINAL Tiempo invertido: 29 minutos. Intentos: 1

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,32 @@
 function clasificaElementos(index) {
-    return [0,0,0];
+    var positivo = 0;
+    var negativo = 0;
+    var cero = 0;
+    for(indice of index){
+
+        if(indice > 0){
+            positivo++;
+        }
+        else{
+            if(indice < 0){
+                
+                negativo++;
+            }else{
+                
+                cero++;
+            }
+        }
+    }
+    var positiveFix = parseFloat(positivo/index.length);
+    var negativoFix = parseFloat(negativo/index.length);
+    var ceroFix = parseFloat(cero/index.length);
+    resultado = new Array(positiveFix.toFixed(4), negativoFix.toFixed(4), ceroFix.toFixed(4));
+    console.log(resultado);
+    return resultado;
+    
 }
+
+
+
+
 module.exports = clasificaElementos;
